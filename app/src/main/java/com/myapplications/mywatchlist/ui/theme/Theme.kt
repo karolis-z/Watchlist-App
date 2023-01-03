024f1c3b82b2c2244a96ed381b72a207644d702b
karolis-z <zaveckas.k@gmail.com>
2023-01-03 15:09:41 +0200

    Set up Theme Colors and Typography.

--- app/src/main/java/com/myapplications/mywatchlist/ui/theme/Theme.kt
+++ app/src/main/java/com/myapplications/mywatchlist/ui/theme/Theme.kt
@@ -13,35 +13,78 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = md_light_primary,
+    onPrimary = md_light_onPrimary,
+    primaryContainer = md_light_primaryContainer,
+    onPrimaryContainer = md_light_onPrimaryContainer,
+    secondary = md_light_secondary,
+    onSecondary = md_light_onSecondary,
+    secondaryContainer = md_light_secondaryContainer,
+    onSecondaryContainer = md_light_onSecondaryContainer,
+    tertiary = md_light_tertiary,
+    onTertiary = md_light_onTertiary,
+    tertiaryContainer = md_light_tertiaryContainer,
+    onTertiaryContainer = md_light_onTertiaryContainer,
+    error = md_light_error,
+    errorContainer = md_light_errorContainer,
+    onError = md_light_onError,
+    onErrorContainer = md_light_onErrorContainer,
+    background = md_light_background,
+    onBackground = md_light_onBackground,
+    surface = md_light_surface,
+    onSurface = md_light_onSurface,
+    surfaceVariant = md_light_surfaceVariant,
+    onSurfaceVariant = md_light_onSurfaceVariant,
+    outline = md_light_outline,
+    inverseOnSurface = md_light_inverseOnSurface,
+    inverseSurface = md_light_inverseSurface,
+    inversePrimary = md_light_inversePrimary,
+    surfaceTint = md_light_surfaceTint,
+    outlineVariant = md_light_outlineVariant,
+    scrim = md_light_scrim,
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+
+private val DarkColorScheme = darkColorScheme(
+    primary = md_dark_primary,
+    onPrimary = md_dark_onPrimary,
+    primaryContainer = md_dark_primaryContainer,
+    onPrimaryContainer = md_dark_onPrimaryContainer,
+    secondary = md_dark_secondary,
+    onSecondary = md_dark_onSecondary,
+    secondaryContainer = md_dark_secondaryContainer,
+    onSecondaryContainer = md_dark_onSecondaryContainer,
+    tertiary = md_dark_tertiary,
+    onTertiary = md_dark_onTertiary,
+    tertiaryContainer = md_dark_tertiaryContainer,
+    onTertiaryContainer = md_dark_onTertiaryContainer,
+    error = md_dark_error,
+    errorContainer = md_dark_errorContainer,
+    onError = md_dark_onError,
+    onErrorContainer = md_dark_onErrorContainer,
+    background = md_dark_background,
+    onBackground = md_dark_onBackground,
+    surface = md_dark_surface,
+    onSurface = md_dark_onSurface,
+    surfaceVariant = md_dark_surfaceVariant,
+    onSurfaceVariant = md_dark_onSurfaceVariant,
+    outline = md_dark_outline,
+    inverseOnSurface = md_dark_inverseOnSurface,
+    inverseSurface = md_dark_inverseSurface,
+    inversePrimary = md_dark_inversePrimary,
+    surfaceTint = md_dark_surfaceTint,
+    outlineVariant = md_dark_outlineVariant,
+    scrim = md_dark_scrim,
 )
 
 @Composable
 fun MyWatchlistTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    // Dynamic color is available on Android 12+. Disabling now, might implement in the future.
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -54,15 +97,20 @@ fun MyWatchlistTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
+        /* getting the current window by tapping into the Activity */
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            currentWindow.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
+                darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = MyTypography,
         content = content
     )
 }
--- app/src/main/java/com/myapplications/mywatchlist/ui/theme/Type.kt
+++ app/src/main/java/com/myapplications/mywatchlist/ui/theme/Type.kt
@@ -2,33 +2,125 @@ package com.myapplications.mywatchlist.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.myapplications.mywatchlist.R
+
+val MyFonts = FontFamily(
+    Font(resId = R.font.quicksand_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.quicksand_semi_bold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.quicksand_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.quicksand_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.quicksand_light, weight = FontWeight.Light),
+)
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val MyTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = MyFonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 57.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MyFonts,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = MyFonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MyFonts,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
