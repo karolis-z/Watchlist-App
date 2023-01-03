3a044502968fe51e89298cd017554e5ee6224f2a
karolis-z <zaveckas.k@gmail.com>
2023-01-03 16:06:17 +0200

    Created TitleItem.kt model to be used in domain and ui for displaying movies and tv shows in a list. Along with Genre and TitleType  data and enum classes.

---
+++ app/src/main/java/com/myapplications/mywatchlist/data/entities/TitleItem.kt
@@ -0,0 +1,26 @@
+package com.myapplications.mywatchlist.data.entities
+
+import java.time.LocalDateTime
+
+data class TitleItem(
+    val id: Long,
+    val name: String,
+    val type: TitleType,
+    val mediaId: Long,
+    val overview: String,
+    val posterLink: String,
+    val genres: List<Genre>,
+    val firstAirDate: LocalDateTime,
+    val voteCount: Long,
+    val voteAverage: Double,
+)
+
+enum class TitleType {
+    MOVIE,
+    TV
+}
+
+data class Genre(
+    val id: Long,
+    val name: String
+)
