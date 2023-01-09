cf730a9f3ed30157a7ad336f6ef2177bee304f4f
karolis-z <zaveckas.k@gmail.com>
2023-01-09 12:52:22 +0200

    Added FAB in SearchScreen.kt that will focus on the search field.

--- app/src/main/java/com/myapplications/mywatchlist/ui/components/TitleItemsList.kt
+++ app/src/main/java/com/myapplications/mywatchlist/ui/components/TitleItemsList.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -17,12 +19,16 @@ fun TitleItemsList(
     titleItems: List<TitleItem>,
     placeholderImage: Painter,
     onWatchlistClicked: (TitleItem) -> Unit,
+    modifier: Modifier = Modifier,
+    state: LazyListState = rememberLazyListState(),
     verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
     LazyColumn(
         verticalArrangement = verticalArrangement,
-        contentPadding = contentPadding
+        contentPadding = contentPadding,
+        state = state,
+        modifier = modifier
     ) {
         items(
             items = titleItems,
--- app/src/main/java/com/myapplications/mywatchlist/ui/search/SearchScreen.kt
+++ app/src/main/java/com/myapplications/mywatchlist/ui/search/SearchScreen.kt
@@ -3,10 +3,13 @@ package com.myapplications.mywatchlist.ui.search
 import androidx.compose.animation.*
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -14,6 +17,8 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -47,67 +52,83 @@ fun SearchScreen() {
     val focusManager = LocalFocusManager.current
     val focusRequester = remember { FocusRequester() }
 
-    Column(modifier = Modifier.padding(vertical = 10.dp, horizontal = 5.dp)) {
-        SearchTextField(
-            onSearchClicked = { viewModel.searchTitleClicked(it) } ,
-            focusManager = focusManager,
-            keyboardController = keyboardController,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        if (!uiState.value.isSearchFinished && !uiState.value.isLoading) {
-            Box(
+    val listState = rememberLazyListState()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.padding(vertical = 10.dp, horizontal = 5.dp)) {
+            SearchTextField(
+                onSearchClicked = { viewModel.searchTitleClicked(it) },
+                focusManager = focusManager,
+                focusRequester = focusRequester,
+                keyboardController = keyboardController,
                 modifier = Modifier
-                    .fillMaxSize()
-                    .weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = stringResource(id = R.string.search_not_searched_yet),
-                    style = MaterialTheme.typography.headlineMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
-        } else if (uiState.value.isLoading) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                LoadingCircle()
-            }
-        } else if (uiState.value.isNoData) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(15.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = stringResource(id = R.string.search_nothing_found),
-                    style = MaterialTheme.typography.headlineMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
-        } else {
-            val titleItems = uiState.value.titleItems
-            if (titleItems != null) {
-                TitleItemsList(
-                    titleItems = titleItems,
-                    placeholderImage = placeholderImage,
-                    onWatchlistClicked = { viewModel.onWatchlistClicked(it) }
-                )
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            if (!uiState.value.isSearchFinished && !uiState.value.isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.search_not_searched_yet),
+                        style = MaterialTheme.typography.headlineMedium,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else if (uiState.value.isLoading) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    LoadingCircle()
+                }
+            } else if (uiState.value.isNoData) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(15.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.search_nothing_found),
+                        style = MaterialTheme.typography.headlineMedium,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else {
+                val titleItems = uiState.value.titleItems
+                if (titleItems != null) {
+                    TitleItemsList(
+                        titleItems = titleItems,
+                        placeholderImage = placeholderImage,
+                        onWatchlistClicked = { viewModel.onWatchlistClicked(it) },
+                        state = listState
+                    )
+                }
             }
         }
+        SearchFAB(
+            isFabVisible = listState.isScrollingUp(),
+            onFabClicked = { focusRequester.requestFocus() } ,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 20.dp, end = 20.dp)
+        )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class,
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class,
     ExperimentalAnimationApi::class
 )
 @Composable
 fun SearchTextField(
     onSearchClicked: (String) -> Unit,
     focusManager: FocusManager,
+    focusRequester: FocusRequester,
     keyboardController: SoftwareKeyboardController?,
     modifier: Modifier = Modifier
 ) {
@@ -117,7 +138,11 @@ fun SearchTextField(
     TextField(
         value = searchValue,
         onValueChange = { searchValue = it },
-        modifier = modifier,
+        modifier = modifier
+            .focusRequester(focusRequester)
+            .onFocusChanged {
+                if (it.isFocused) keyboardController?.show()
+            },
         label = { Text(text = stringResource(id = R.string.search_searchfield_label)) },
         colors = TextFieldDefaults.textFieldColors(
             containerColor = MaterialTheme.colorScheme.secondaryContainer,
@@ -152,6 +177,27 @@ fun SearchTextField(
     )
 }
 
+@Composable
+fun SearchFAB(
+    isFabVisible: Boolean,
+    onFabClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AnimatedVisibility(
+        visible = isFabVisible,
+        modifier = modifier,
+        enter = fadeIn() + slideInHorizontally(initialOffsetX = {fullWidth -> fullWidth }),
+        exit = fadeOut() + slideOutHorizontally(targetOffsetX = {fullWidth -> fullWidth })
+    ) {
+        FloatingActionButton(onClick = onFabClicked) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(id = R.string.cd_search_icon)
+            )
+        }
+    }
+}
+
 @OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
@@ -160,7 +206,29 @@ fun SearchTextFieldPreview() {
         onSearchClicked = {},
         focusManager = LocalFocusManager.current,
         keyboardController = null,
+        focusRequester = FocusRequester()
     )
+}
+
+/**
+ * Returns whether the lazy list is currently scrolling up.
+ */
+@Composable
+private fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }
 
 
