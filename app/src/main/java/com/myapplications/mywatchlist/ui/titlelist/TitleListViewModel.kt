55249cb37d3e68f2825bd19fbdd9c20b1e5a8c81
Karolis <34347984+karolis-z@users.noreply.github.com>
2023-02-02 18:11:04 +0200

    WIP: Title List Filter Created TitleListFilter.kt Changed TitleListViewModel.kt to emit state based on remote data state as well as filter state. Implemented applyFilter method, but still need to test.

--- app/src/main/java/com/myapplications/mywatchlist/ui/titlelist/TitleListViewModel.kt
+++ app/src/main/java/com/myapplications/mywatchlist/ui/titlelist/TitleListViewModel.kt
@@ -9,11 +9,12 @@ import com.myapplications.mywatchlist.domain.entities.TitleItemFull
 import com.myapplications.mywatchlist.domain.repositories.TitlesManager
 import com.myapplications.mywatchlist.domain.result.ResultOf
 import com.myapplications.mywatchlist.ui.NavigationArgument
+import com.myapplications.mywatchlist.ui.entities.TitleListFilter
 import com.myapplications.mywatchlist.ui.entities.TitleListType
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 import javax.inject.Inject
 
 private const val TAG = "TITLE_LIST_VIEWMODEL"
@@ -24,7 +25,30 @@ class TitleListViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    val uiState: MutableStateFlow<TitleListUiState> = MutableStateFlow(TitleListUiState.Loading)
+
+    private val titleListState: MutableStateFlow<TitleListUiState> =
+        MutableStateFlow(TitleListUiState.Loading)
+    private val filterState: MutableStateFlow<TitleListFilter> = MutableStateFlow(TitleListFilter())
+
+    val uiState: StateFlow<TitleListUiState> =
+        combine(titleListState, filterState) { titleListState, filter ->
+            when (titleListState) {
+                is TitleListUiState.Error -> titleListState
+                TitleListUiState.Loading -> titleListState
+                is TitleListUiState.Ready -> {
+                    TitleListUiState.Ready(
+                        titleItems = applyFilter(
+                            titles = titleListState.titleItems,
+                            filter = filter
+                        )
+                    )
+                }
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = TitleListUiState.Loading
+        )
 
     private var titleListType: TitleListType? = null
 
@@ -33,13 +57,66 @@ class TitleListViewModel @Inject constructor(
         getTitleList()
     }
 
+    private fun applyFilter(
+        titles: List<TitleItemFull>,
+        filter: TitleListFilter
+    ): List<TitleItemFull> {
+        var titlesList = titles.apply {
+            // Apply TitleType filter
+            if (filter.titleType != null) this.filter { it.type == filter.titleType }
+            // Apply score range filter
+            if (filter.scoreRange != Pair(0, 10)) this.filter {
+                (it.voteAverage >= filter.scoreRange.first) &&
+                        (it.voteAverage <= filter.scoreRange.second)
+            }
+        }
+        // Apply years range filter
+        if (filter.yearsRange != Pair(1900, LocalDate.now().year)) {
+            val showFutureTitles = filter.scoreRange.second == LocalDate.now().year
+
+            val titlesWithNoReleaseDate = titlesList.filter { it.releaseDate == null }
+            val titlesWithReleaseDate = if (titlesWithNoReleaseDate.isNotEmpty()) {
+                titlesList.minus(titlesWithNoReleaseDate.toSet())
+            } else {
+                titlesList
+            }.filter {
+                /* Non-null assertion here because we already filtered out items with
+                null release date */
+                if (showFutureTitles) {
+                    it.releaseDate!!.year >= filter.scoreRange.first
+                } else {
+                    (it.releaseDate!!.year >= filter.scoreRange.first) &&
+                            (it.releaseDate.year <= filter.scoreRange.second)
+                }
+            }
+            titlesList = titlesWithReleaseDate + titlesWithNoReleaseDate
+        }
+
+        // TODO: Test this filter
+        // titlesList = titlesList.filter { title -> filter.genres.any { genre -> title.genres.contains(genre) }}
+
+        if (filter.genres.isNotEmpty()) {
+            val filteredTitles = mutableListOf<TitleItemFull>()
+            titlesList.forEach outer@{ title ->
+                title.genres.forEach inner@{ genre ->
+                    if (filter.genres.contains(genre)) {
+                        filteredTitles.add(title)
+                        return@outer
+                    }
+                }
+            }
+            titlesList = filteredTitles
+        }
+        return titlesList
+    }
+
     private fun getTitleListType(){
-        uiState.update { TitleListUiState.Loading }
+        titleListState.update { TitleListUiState.Loading }
         try {
             val titleListTypeString = savedStateHandle.get<String>(NavigationArgument.TITLE_LIST_TYPE.value)
             if (titleListTypeString == null) {
                 // Unknown why it failed to parse the provided title, so should show general error
-                uiState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
+                titleListState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
                 return
             } else {
                 titleListType = TitleListType.valueOf(titleListTypeString)
@@ -47,17 +124,17 @@ class TitleListViewModel @Inject constructor(
         } catch (e: Exception) {
             Log.e(TAG, "Failed to get TitleListType from savedStateHandle. Reason: $e", e)
             // Unknown why it failed to parse the provided title, so should show general error
-            uiState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
+            titleListState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
         }
     }
 
     private fun getTitleList() {
-        uiState.update { TitleListUiState.Loading }
+        titleListState.update { TitleListUiState.Loading }
         viewModelScope.launch {
             val result = when (titleListType) {
                 null -> {
                     Log.e(TAG, "getTitleList: titleListType is null. Unable to infer which list to get")
-                    uiState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
+                    titleListState.update { TitleListUiState.Error(TitleListError.UNKNOWN) }
                     return@launch
                 }
                 TitleListType.Trending -> titlesManager.getTrendingTitles()
@@ -66,14 +143,14 @@ class TitleListViewModel @Inject constructor(
                 TitleListType.UpcomingMovies -> titlesManager.getUpcomingMovies()
             }
             when (result) {
-                is ResultOf.Failure -> uiState.update {
+                is ResultOf.Failure -> titleListState.update {
                     TitleListUiState.Error(
                         error = getErrorFromResultThrowable(
                             result.throwable
                         )
                     )
                 }
-                is ResultOf.Success -> uiState.update {
+                is ResultOf.Success -> titleListState.update {
                     TitleListUiState.Ready(titleItems = result.data)
                 }
             }
